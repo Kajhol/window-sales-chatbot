4b4829d3081b5a28c41a5febfd5bb471b4be0065
Kajhol <kajetanhol@gmail.com>
2025-12-13 11:47:16 +0100

    Pierwsza wersja

---
+++ test.py
@@ -0,0 +1,9 @@
+import nltk
+import sklearn
+import numpy
+
+print("NLTK wersja:", nltk.__version__)
+print("Scikit-learn wersja:", sklearn.__version__)
+print("NumPy wersja:", numpy.__version__)
+print("")
+print("Wszystko działa poprawnie!")
